a9119d72d18db4e87f5caf22fea54664815783e0
rajsingh51 <Raj.Singh@payrange.com>
2026-04-23 16:27:09 +0530

    add winston logger

--- auth-service/src/server.ts
+++ auth-service/src/server.ts
@@ -1,15 +1,16 @@
 import app from './app';
 import { Config } from './config';
+import logger from './config/logger';
 
 const startServer = () => {
   const PORT = Config.PORT;
 
   const server = app.listen(PORT, () => {
-    console.log(`Auth service is running on port ${PORT}`);
+    logger.info(`Auth service is running on port ${PORT}`);
   });
 
   server.on('error', (error) => {
-    console.error(`Error starting auth service: ${error.message}`);
+    logger.error(`Error starting auth service: ${error.message}`);
     process.exit(1);
   });
 };
