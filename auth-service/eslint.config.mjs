aa2bf924d875dc4c73c773c9fddf5fedada19390
rajsingh51 <Raj.Singh@payrange.com>
2026-04-23 15:26:27 +0530

    add eslint

---
+++ auth-service/eslint.config.mjs
@@ -0,0 +1,20 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  { ignores: ['dist/**', 'node_modules/**', 'eslint.config.mjs'] },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      // 'no-console': 'warn',
+    },
+  },
+]);
