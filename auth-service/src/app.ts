afc0a506e04f0eff8f0174f01454157820af2b11
rajsingh51 <Raj.Singh@payrange.com>
2026-04-24 11:46:22 +0530

    refactor error handling

--- auth-service/src/app.ts
+++ auth-service/src/app.ts
@@ -1,7 +1,6 @@
-import express, { NextFunction, Request, Response } from 'express';
-import { HttpError } from 'http-errors';
-import logger from './config/logger';
+import express from 'express';
 import router from './routes';
+import errorHandler from './middlewares/errorHandler';
 
 const app = express();
 
@@ -13,19 +12,6 @@ app.get('/health', (_req, res) => {
 
 app.use(router);
 
-app.use((err: HttpError, _req: Request, res: Response, _next: NextFunction) => {
-  logger.error(err.message);
-  const statusCode = err.statusCode || 500;
-  res.status(statusCode).json({
-    errors: [
-      {
-        type: err.name,
-        msg: err.message,
-        path: '',
-        location: '',
-      },
-    ],
-  });
-});
+app.use(errorHandler);
 
 export default app;
---
+++ auth-service/src/middlewares/errorHandler.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from 'express';
+import { HttpError } from 'http-errors';
+import logger from '../config/logger';
+
+const errorHandler = (
+  err: HttpError,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  const statusCode = err.statusCode || 500;
+
+  logger.error(err.message);
+
+  res.status(statusCode).json({
+    errors: [
+      {
+        type: err.name,
+        msg: err.message,
+        path: '',
+        location: '',
+      },
+    ],
+  });
+};
+
+export default errorHandler;
